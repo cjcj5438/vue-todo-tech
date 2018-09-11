153eb3495f94ae641b80b4814c714dac2148962e
cjcj5438 <1002931123@qq.com>
2018-09-11 08:02:48 +0800

    log time

--- client/create-app.js
+++ client/create-app.js
@@ -1,2 +1,11 @@
 // 因为每次服务端渲染都要去渲染一个新的app,我不能有上一次渲染过的对象,再去下一次渲染,会有影响
-// 5-3 08:46
+/*import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Vuex from 'vuex'
+
+import App from './app.vue'
+import createStore from './store/store'
+import createRouter from './config/router'
+
+import './assets/styles/global.styl'*/
+// 5-3 009.20
